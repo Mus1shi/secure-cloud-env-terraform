57c88ee9235efbe4c4b478ea8e2270c326e236e3
Tom <tomvla49@gmail.com>
2025-09-18 12:59:46 +0200

    module updated wit my_ip_cidr

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ module "network" {
   vnet_cidr           = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
-  my_ip_cidr          = "<TON.IP.PUBLIQUE>/32"
+  my_ip_cidr          = var.my_ip_cidr  
 }
