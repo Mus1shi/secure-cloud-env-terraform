3ad2f52a79f04baf32e4744ebf64c7fd6898fac0
Tom <tomvla49@gmail.com>
2025-09-18 12:44:25 +0200

    module added to the main.tf from root

--- main.tf
+++ main.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-secure"
   location = "westeurope"
 }
+
+module "network" {
+  source = "./modules/network"  
+
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  project_name        = "secureenv"        
+  vnet_cidr           = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+  my_ip_cidr          = "<TON.IP.PUBLIQUE>/32" 
+}
