0eb1aeb7cab2bf24cafe9d9a61da6be28efeb57d
Tom <tomvla49@gmail.com>
2025-09-18 12:19:07 +0200

    variable added to the file .tf in modules folder

---
+++ modules/network/variables.tf
@@ -0,0 +1,40 @@
+variable "resource_group_name" {
+    description = "The name of the resource group where resources will be created"
+    type = string
+}
+
+variable "location" {
+    description = "Azure region for all resources"
+    type = string
+}
+
+variable "project_name" {
+   description = "Project prefix used for naming resources"
+   type = string
+}
+
+variable "vnet_cidr" {
+  description = "Address space for the virtual network"
+  type = string
+}
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  type = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "my_ip_cidr" {
+  description = "Your public IP in CIDR notation (/32) for SSH access"
+  type = string
+
+  validation {
+    condition = can(regex("\\/32$", var.my_ip_cidr))
+    error_message = "The value of my_ip_cidr must end with /32."
+  }
+}
