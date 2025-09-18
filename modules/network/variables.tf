b5334bb3e6cfe231221e4910d4aca225523f6978
Tom <tomvla49@gmail.com>
2025-09-18 12:42:57 +0200

    outputs in outputs.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -16,11 +16,13 @@ variable "project_name" {
 variable "vnet_cidr" {
   description = "Address space for the virtual network"
   type = string
+  default     = "10.0.0.0/16"
+
 }
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
   type = string
-  default     = "10.0.2.0/24"
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
