27dc0bfd63f07ee4e654ece17dc1f23f98b0e460
Tom <tomvla49@gmail.com>
2025-09-18 12:48:45 +0200

    output from root addded

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "network" {
-  source = "./modules/network"  
+  source = "./modules/network"
 
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  project_name        = "secureenv"        
+  project_name        = "secureenv"
   vnet_cidr           = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
-  my_ip_cidr          = "<TON.IP.PUBLIQUE>/32" 
+  my_ip_cidr          = "<TON.IP.PUBLIQUE>/32"
 }
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "public_subnet_id" {
+  value = module.network.public_subnet_id
+}
